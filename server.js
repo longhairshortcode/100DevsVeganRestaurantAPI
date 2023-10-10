e7872418320d98c65d2c74b049a34f11c4696c79
Rona <longhairshortcode@gmail.com>
2023-10-09 22:52:47 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const veganrestaurants = {
+  'sage' : {
+    'restaurant': 'sage',
+  'city': 'pasadena',
+  'parking': 'free-with-validation'
+},
+
+'kitchen mouse' : {
+  'restaurant': 'Kitchen Mouse',
+  'city': 'Highland Park',
+  'parking': 'street and meter parking'
+},
+
+'erewhon' : {
+  'restaurant': 'Erewhon',
+  'city': 'Studio City',
+  'parking': 'free for 90 mins.'
+},
+
+'unknown' : {
+  'restaurant': 'unknown',
+  'city': 'unknown',
+  'parking': 'unknown'
+
+}
+}
+
+app.get('/', (request, response) =>{
+  response.sendFile(__dirname + '/index.html')  
+}),
+
+app.get('/api/:name', (request, response) =>{
+  const restName = request.params.name.toLowerCase()
+  if(veganrestaurants[restName]){
+    response.json(veganrestaurants[restName])
+  }else{
+    response.json(veganrestaurants['unknown'])
+  }
+}),
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}`)
+})
